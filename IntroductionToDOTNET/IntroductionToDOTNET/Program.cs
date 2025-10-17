bd477f29b1d98223717abb5f20366d742cf43ce0
User <User@DESKTOP-QHG18FL>
2025-10-17 21:48:56 +0300

    Intro

---
+++ IntroductionToDOTNET/IntroductionToDOTNET/Program.cs
@@ -0,0 +1,46 @@
+﻿//IntroductionToDOTNET
+//#define CONSOLE
+using System;	//#include
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroductionToDOTNET
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+#if CONSOLE
+			Console.WriteLine("Hello World!");
+			Console.BackgroundColor = ConsoleColor.DarkBlue;
+			//Console.ForegroundColor = ConsoleColor.DarkRed;
+			Console.Write("Hello .NET\t");
+			Console.WriteLine();
+			Console.Title = "Introduction to .NET";
+			Console.Beep(37, 2000);
+			Console.CursorLeft = 25;
+			Console.CursorTop = 5;
+			Console.WriteLine("SetCursorPosition");
+			Console.SetCursorPosition(22, 8);
+			Console.WriteLine("Another position");
+			Console.ResetColor();  
+#endif
+
+			Console.Write("Введите Ваше имя:");
+			string firstName = Console.ReadLine();
+
+			Console.Write("Введите Вашу фамилию: ");
+			string lastName = Console.ReadLine();
+
+			Console.Write("Введите Ваш возраст: ");
+			int age = Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine(lastName + " " + firstName + " " + age);  //Конкатенация строк
+			Console.WriteLine(String.Format("{0} {1} {2}", lastName, firstName, age));  //Форматирование строк
+			Console.WriteLine($"{lastName} {firstName} {age}");		//Интерполяция строк
+
+		}
+	}
+}
---
+++ ReadMe.txt
@@ -0,0 +1,4 @@
+﻿https://github.com/okovtun/PV_521_CS.git
+https://www.youtube.com/playlist?list=PLeqyOOqxeiIPna7oEwBxnlj3yEEZYFBWx
+
+https://www.ixbt.com/cpu/cpu-digest-2009.shtml
