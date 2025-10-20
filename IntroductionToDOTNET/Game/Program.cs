476f57edf41e04e4055bb7788691c5dfb42e495d
User <User@DESKTOP-QHG18FL>
2025-10-20 21:53:43 +0300

    DataTypes

--- IntroductionToDOTNET/Game/Program.cs
+++ IntroductionToDOTNET/Game/Program.cs
@@ -1,15 +1,25 @@
-﻿https://github.com/okovtun/PV_521_CS.git
-https://www.youtube.com/playlist?list=PLeqyOOqxeiIPna7oEwBxnlj3yEEZYFBWx
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-https://www.ixbt.com/cpu/cpu-digest-2009.shtml
+/*
 
 TODO:
 1. Используя операторы sizeof(), typeof() и классы-обвертки вывести на экран информацию обо всех числовых типа;
 2. В Solution 'IntroductionToDOTNET' добавить проект 'Game', и в этом проекте на экране отображается символ @, ^. *, ██,
    и этот символ перемещается по экрану при нажатии клавиш WASD либо клавиш курсора;
 
-DONE:
-1. Вывести все фигуры из файла Geometry.txt:
-   https://github.com/okovtun/PV_521_CS/blob/master/IntroductionToDOTNET/Geometry.txt
-2. https://github.com/okovtun/PV_521_CS/blob/master/IntroductionToDOTNET/HardChess.jpg
-3. Залить Solution на GitHub и прислать ссылку на репозиторий;
+ */
+
+namespace Game
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Game");
+		}
+	}
+}
