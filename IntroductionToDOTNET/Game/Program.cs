cb4ee545adb17eb094ef48b2cadbdd59a3829789
User <User@DESKTOP-QHG18FL>
2025-10-24 21:49:50 +0300

    OOP

--- IntroductionToDOTNET/Game/Program.cs
+++ IntroductionToDOTNET/Game/Program.cs
@@ -2,24 +2,62 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
-
-/*
-
-TODO:
-1. Используя операторы sizeof(), typeof() и классы-обвертки вывести на экран информацию обо всех числовых типа;
-2. В Solution 'IntroductionToDOTNET' добавить проект 'Game', и в этом проекте на экране отображается символ @, ^. *, ██,
-   и этот символ перемещается по экрану при нажатии клавиш WASD либо клавиш курсора;
-
- */
 
 namespace Game
 {
-	class Program
+	internal class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Game");
+			string simbol = "--*--";
+			int x = 0, y = 0;
+
+			Console.CursorVisible = false;
+			Write(simbol, x, y);
+			ConsoleKey key;
+			do
+			{
+				key = Console.ReadKey(true).Key;
+				switch (key)
+				{
+					case ConsoleKey.LeftArrow:
+						if (x > 0) x--; break;
+					case ConsoleKey.RightArrow:
+						x++; break;
+					case ConsoleKey.UpArrow:
+						if (y > 0) y--; break;
+					case ConsoleKey.DownArrow:
+						y++; break;
+				}
+
+				//if (Console.ReadKey().Key == ConsoleKey.LeftArrow || Console.ReadKey().Key == ConsoleKey.A)
+				//{ if (x > 0) x--; }
+				//else if (Console.ReadKey().Key == ConsoleKey.RightArrow || Console.ReadKey().Key == ConsoleKey.D)
+				//{ x++; }
+				//else if (Console.ReadKey().Key == ConsoleKey.UpArrow || Console.ReadKey().Key == ConsoleKey.W)
+				//{ if (y > 0) y--; }
+				//else if (Console.ReadKey().Key == ConsoleKey.DownArrow || Console.ReadKey().Key == ConsoleKey.S)
+				//{ y++; }
+				int currentX = Console.CursorLeft;
+				int currentY = Console.CursorTop;
+				Write(simbol, x, y);
+				Console.SetCursorPosition(0, 0);
+				Console.WriteLine($"X = {currentX}, Y = {currentY}");
+				Console.SetCursorPosition(x, y);
+			}
+			while (key != ConsoleKey.Escape);
 		}
+		static void Write(string simbol, int x, int y)
+		{
+			if (x >= 0 && y >= 1)
+			{
+				Console.Clear();
+				Console.SetCursorPosition(x, y);
+				Console.WriteLine(simbol);
+			}
+		}
+
 	}
 }
---
+++ OOP/Point/Point.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Point
+{
+	class Point
+	{
+		//private, public, protected,
+		//internal - поля доступны в любом месте сборки
+		//protected internal - поля доступны только для дочерних классов в пределах сборки
+		//double x;
+		//double y;
+		////Properties - это синтаксическая конструкция, которая объединяет Get и Set метод,
+		////			   и позволяет использовать их как обычную переменную в классе.
+		///// Properties /////////////////////////////////////////////////////////////////////////////
+		//public double X
+		//{
+		//	get
+		//	{
+		//		return x;
+		//	}
+		//	set
+		//	{
+		//		if (value > 100) value = 100;
+		//		x = value;	//value - возвращает принимаемое значение.
+		//	}
+		//}
+		//public double Y
+		//{
+		//	get
+		//	{
+		//		return y;
+		//	}
+		//	set
+		//	{
+		//		if (value > 100) value = 100;
+		//		y = value;
+		//	}
+		//}
+		// EndProperties /////////////////////////////////////////////////////////////////////////////
+		public double X { get; set; }	//Auto properties
+		public double Y { get; set; }   //Auto properties
+										//public double GetX()
+										//{
+										//	return x;
+										//}
+										//public double GetY()
+										//{
+										//	return y;
+										//}
+										//public void SetX(double x)
+										//{
+										//	this.x = x;
+										//}
+										//public void SetY(double y)
+										//{
+										//	this.y = y;
+										//}
+
+		/*
+		Garbage collector
+		*/
+		public Point(double x = 0, double y = 0)
+		{
+			X = x;
+			Y = y;
+			Console.WriteLine($"Constructor:\t{this.GetHashCode()}");
+		}
+		public Point(Point other)
+		{
+			this.X = other.X;
+			this.Y = other.Y;
+			Console.WriteLine($"CopyConstructor:{this.GetHashCode()}");
+		}
+		~Point()
+		{
+			Console.WriteLine($"Destructor:\t{this.GetHashCode()}");
+		}
+		//			Operators:
+		public static Point operator+(Point left, Point right)
+		{
+			return new Point
+				(
+					left.X+right.X,
+					left.Y+right.Y
+				);
+		}
+		public void Info()
+		{
+			Console.WriteLine($"X = {X}, Y = {Y}");
+		}
+	}
+}
