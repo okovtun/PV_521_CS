7a70320603cc9c20a8a24d53191f1fd8e4ece4bf
User <User@DESKTOP-QHG18FL>
2025-11-08 10:06:56 +0300

    Abstract Geometry in progress

---
+++ InheritanceCS/AbstractGeometry/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Runtime.InteropServices;   //DllImport
+using System.Windows.Forms;
+
+namespace AbstractGeometry
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			IntPtr hwnd = GetConsoleWindow();
+			Graphics graphics = Graphics.FromHwnd(hwnd);
+			System.Drawing.Rectangle window_rect = new System.Drawing.Rectangle
+				(
+				Console.WindowLeft, Console.WindowTop,
+				Console.WindowWidth, Console.WindowHeight
+				);
+			PaintEventArgs e = new PaintEventArgs(graphics, window_rect);
+			//e.Graphics.DrawRectangle(new Pen(Color.Red), 300, 100, 500, 300);
+
+			Rectangle rectangle = new Rectangle(100, 40, 300, 50, 3, Color.AliceBlue);
+			rectangle.Info(e);
+		}
+		[DllImport("kernel32.dll")]
+		public static extern IntPtr GetConsoleWindow();
+	}
+}
---
+++ InheritanceCS/AbstractGeometry/Rectangle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace AbstractGeometry
+{
+	class Rectangle:Shape
+	{
+		double width;
+		double height;
+		public double Width
+		{
+			get => width;
+			set => width = FilterSize(value);
+		}
+		public double Height
+		{
+			get => height;
+			set => height = FilterSize(value);
+		}
+		public Rectangle
+			(
+			double width, double height,
+			int startX, int startY, int lineWidth, Color color
+			):base(startX, startY, lineWidth, color)
+		{
+			Width = width;
+			Height = height;
+		}
+		public override double GetArea() => Width * Height;
+		public override double GetPerimeter() => 2 * (Width + Height);
+		public override void Draw(System.Windows.Forms.PaintEventArgs e)
+		{
+			Pen pen = new Pen(Color);
+			SolidBrush brush = new SolidBrush(Color);
+			e.Graphics.DrawRectangle(pen, StartX, StartY, (float)Width, (float)Height);
+			e.Graphics.FillRectangle(brush, StartX, StartY, (float)Width, (float)Height);
+			//https://learn.microsoft.com/en-us/dotnet/desktop/winforms/advanced/how-to-draw-a-filled-rectangle-on-a-windows-form
+		}
+		public override void Info(System.Windows.Forms.PaintEventArgs e)
+		{
+			Console.WriteLine(this.GetType().ToString().Split('.').Last()+":");
+			Console.WriteLine($"Ширина:{Width}");
+			Console.WriteLine($"Высота:{Height}");
+			base.Info(e);
+		}
+	}
+}
