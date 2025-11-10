76be05b3f478934d8b77b620d5c0ded92fcec0c3
User <User@DESKTOP-QHG18FL>
2025-11-10 20:56:01 +0300

    Triangles in progress

--- InheritanceCS/AbstractGeometry/Program.cs
+++ InheritanceCS/AbstractGeometry/Program.cs
@@ -25,6 +25,19 @@ namespace AbstractGeometry
 
 			Rectangle rectangle = new Rectangle(100, 40, 300, 50, 3, Color.AliceBlue);
 			rectangle.Info(e);
+
+			Square square = new Square(50, 500, 50, 5, Color.Red);
+			square.Info(e);
+
+			Circle circle = new Circle(100, 700, 50, 5, Color.Yellow);
+			circle.Info(e);
+
+			IsoscelesTriangle iso = new IsoscelesTriangle(75, 150, 400, 200, 3, Color.Green);
+			iso.Info(e);
+
+			EquilateralTriangle equ = new EquilateralTriangle(50, 550, 200, 4, Color.Green);
+			equ.Info(e);
+
 		}
 		[DllImport("kernel32.dll")]
 		public static extern IntPtr GetConsoleWindow();
